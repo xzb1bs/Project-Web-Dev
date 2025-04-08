9589ea591c69b68d3c73f64bfd1791ba38cab49a
Kim TImur <k_timur@kbtu.kz>
2025-04-08 19:12:58 +0500

    home back

--- Front/src/app/pages/login/login.component.ts
+++ Front/src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { DataService } from '../../data.service';
 import { FormsModule } from '@angular/forms'; // импортируем для ngModel
@@ -15,19 +16,26 @@ export class LoginComponent implements OnInit {
   password: string = '';
   email:string='';
 
-  constructor(private router: Router,private dataService: DataService) {
+  constructor(private router: Router,private dataService: DataService,
+    private location: Location,
+  ) {
   }
   ngOnInit(): void {
     this.email=this.dataService.getEmail();
   }
 
-  onSubmit() {
+  onSubmit():void {
     if (this.email && this.password) {
       console.log('Email:', this.email);
       console.log('Password:', this.password);
       this.router.navigate(['/dashboard']);
     } else {
-      alert('Please fill in both fields');
+      alert('Please fill in both fields');  
     }
   }
+
+  goBack(event:Event):void {
+    event.preventDefault(); 
+    this.location.back();
+  } 
 }
