82c70a991cc6d8a73f1cbeea959385d6de5bc5dc
Kim TImur <k_timur@kbtu.kz>
2025-04-08 17:40:09 +0500

    Новый фронт

---
+++ Front/src/app/pages/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  onLogin() {
+    const email = this.loginForm.value.email;
+    console.log('Вход с email:', email);
+  }
+}
