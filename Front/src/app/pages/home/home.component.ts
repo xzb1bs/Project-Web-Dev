d9c36b95d6c1dc2752906740b6a348bb33e575a6
slavenmerc <157578846+slavenmerc@users.noreply.github.com>
2025-04-23 18:02:42 +0500

    home page

--- Front/src/app/pages/home/home.component.ts
+++ Front/src/app/pages/home/home.component.ts
@@ -19,7 +19,7 @@ export class HomeComponent {
 
   sendMessage(): void {
     console.log('Message sent:', this.message);
-    this.message = ''; 
+    this.message = ''; // Clear the message after sending
   }
   scrollToFooter(): void {
     const footerElement = document.getElementById('footer');
